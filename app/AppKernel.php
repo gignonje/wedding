7af7d85582194f0d9c481a40b2b827a519e5cc03
Jérémy GIGNON <jeremy.gignon@gmail.com>
2014-12-06 19:32:11 +0100

    Now using BraincraftedBootstrapBundle instead of MopaBootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new Shadow\WeddingBundle\ShadowWeddingBundle(),
         );
 
