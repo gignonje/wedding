8b5b1f912b4544696a7cd39d9e3b4d21d1bf02e8
Jérémy GIGNON <jeremy.gignon@gmail.com>
2015-01-03 22:45:09 +0000

    Replaced hardcoded menu by KnpMenu

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Shadow\WeddingBundle\ShadowWeddingBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Shadow/WeddingBundle/Menu/Builder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Shadow\WeddingBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttributes(['id' => 'topnav', 'class' => 'sf-menu']);
+
+        $menu->addChild('Les mariés', array('route' => 'homepage'));
+        $menu->addChild('Le jour J', array('uri' => '#'));
+        $menu->addChild('Infos pratiques', array('uri' => '#'));
+        $menu->addChild('Le voyage de noces', array('uri' => '#'));
+        $menu->addChild('Contact', array('uri' => '#'));
+
+        return $menu;
+    }
+}
